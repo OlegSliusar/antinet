1b08db42b9cffbdcdf407d12e71000ce587f738c
Oleh Sliusar <olehsliusar@gmail.com>
2016-07-20 06:52:48 +0300

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html: "Hello, AntiNet!"
-  end
-  def goodbye
-    render html: "Goodbye, Society!"
-  end
 end
