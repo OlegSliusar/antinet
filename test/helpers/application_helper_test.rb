030c73627c95d4a999abae5231cd66577f38a491
Oleh Sliusar <olehsliusar@gmail.com>
2016-08-22 17:01:49 +0300

    Finish layout and routes

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -7,9 +7,10 @@ class ApplicationHelperTest < ActionView::TestCase
   end
 
   test "full title helper" do
-    assert_equal full_title,         "#{@base_title}"
-    assert_equal full_title("Help"), "Help | #{@base_title}"
+    assert_equal full_title,            "#{@base_title}"
+    assert_equal full_title("Help"),    "Help | #{@base_title}"
     assert_equal full_title("Contact"), "Contact | #{@base_title}"
-    assert_equal full_title("About"), "About | #{@base_title}"
+    assert_equal full_title("About"),   "About | #{@base_title}"
+    assert_equal full_title("Sign up"), "Sign up | #{@base_title}"
   end
 end
