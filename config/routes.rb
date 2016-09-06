85c3b856639d66f1d1a12e686d7c00e6eb7a276b
Oleh Sliusar <olehsliusar@gmail.com>
2016-09-06 14:51:21 +0300

    Model account activations as a resource

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  #get    '/edit',    to: 'users#edit'
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :account_activations, only: [:edit]
 end
