d7f5a8b15d88dd1b88a5822a4e92630abf02e101
Oleh Sliusar <olehsliusar@gmail.com>
2016-09-07 13:26:53 +0300

    Account activations resource and emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,7 +49,7 @@ class User < ApplicationRecord
 
     # Creates and assigns the activation token and digest.
     def create_activation_digest
-      activation_token  = User.new_token
-      activation_digest = User.digest(activation_token)
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,16 +17,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
    end
 
    test "valid signup information" do
-     get signup_path
-     assert_difference 'User.count', 1 do
-       post signup_path, params: { user: { name: "Foo Bar",
-                                    email: "user@valid.com",
-                                    password:              "foobar",
-                                    password_confirmation: "foobar" } }
-     end
-     follow_redirect!
-     assert_template 'users/show'
-     assert_not flash.empty?
-     assert is_logged_in?
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    #  assert_template 'users/show'
+    #  assert_not flash.empty?
+    #  assert is_logged_in?
    end
 end
